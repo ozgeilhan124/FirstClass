510aab60f6e86119391fcbf68a47ff5db3c2d121
Ozge Ilhan <ozgeilhan@Ozges-MacBook-Air.local>
2020-11-07 11:45:20 -0600

    New message is added

--- src/technoProJavaPractice/FirstClass.java
+++ src/technoProJavaPractice/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("First commit from local master");
-
+        System.out.println("Seconf line of code I am adding");
 	}
 
 }
