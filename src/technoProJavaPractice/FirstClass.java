f475110112328e72712b59e15cb247ddc5814cf1
Ozge Ilhan <ozgeilhan@Ozges-MacBook-Air.local>
2020-11-08 18:55:16 -0600

    new added

--- src/technoProJavaPractice/FirstClass.java
+++ src/technoProJavaPractice/FirstClass.java
@@ -1,11 +1,13 @@
 package technoProJavaPractice;
 
+
 public class FirstClass {
 
 	public static void main(String[] args) {
 		
 		System.out.println("First commit from local master");
         System.out.println("Seconf line of code I am adding");
+        System.out.println("Hello");
 	}
 
 }
